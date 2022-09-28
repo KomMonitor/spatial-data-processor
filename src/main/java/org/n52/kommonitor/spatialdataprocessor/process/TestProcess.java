4a057cc8e654288cdc65a15035def6ee545eabc2
Jan Speckamp <speckij@posteo.net>
2022-09-28 21:57:10 +0200

    Add locationtech JTS Demo

--- src/main/java/org/n52/kommonitor/spatialdataprocessor/process/TestProcess.java
+++ src/main/java/org/n52/kommonitor/spatialdataprocessor/process/TestProcess.java
@@ -4,6 +4,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.geojson.GeoJsonReader;
 import org.n52.kommonitor.models.IndicatorOverviewType;
 import org.n52.kommonitor.models.SpatialUnitOverviewType;
 import org.n52.kommonitor.models.TestProcessType;
@@ -27,13 +29,27 @@ public class TestProcess implements Process<TestProcessType> {
         UUID spatialUnitId = UUID.fromString("4154115f-3fa8-4fb9-9d7a-593ed0885e6c");
 
         SpatialUnitOverviewType spatialUnit = dataManagementClient.getSpatialUnitOverview(spatialUnitId);
-        LOGGER.info(spatialUnit.toString());
+        //LOGGER.info(spatialUnit.toString());
 
         IndicatorOverviewType indicatorOverview = dataManagementClient.getIndicatorOverview(indicatorId);
-        LOGGER.info(indicatorOverview.toString());
+        //LOGGER.info(indicatorOverview.toString());
 
         ObjectNode spatialUnitGeoJSON = dataManagementClient.getSpatialUnitGeoJSON(indicatorId, spatialUnitId);
-        LOGGER.info(spatialUnitGeoJSON.toString());
+        // LOGGER.info(spatialUnitGeoJSON.toString());
+
+        // Test GeoJSON processing capabilities of locationtech JTS
+        GeoJsonReader reader = new GeoJsonReader();
+        Geometry read = reader.read(spatialUnitGeoJSON.toString());
+
+        StringBuilder b = new StringBuilder();
+        b.append("\n-----\n");
+        b.append("Kowalski, analysis!\n");
+        b.append("It seems they are ");
+        b.append(read.equalsExact(read)? "": "NOT ");
+        b.append("exactly the same Skipper.");
+        b.append("\n-----\n");
+
+        LOGGER.info(b.toString());
 
         Thread.sleep(1000 * 30);
         throw new RuntimeException("not implemented yet!");
