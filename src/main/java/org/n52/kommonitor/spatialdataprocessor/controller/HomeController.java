26f753ee512671ba8c80c40336d8f2f761bfbb2e
Sebastian Drost <s.drost@52north.org>
2022-09-29 13:01:38 +0200

    Add home controller for swagger-ui redirecting

---
+++ src/main/java/org/n52/kommonitor/spatialdataprocessor/controller/HomeController.java
@@ -0,0 +1,30 @@
+package org.n52.kommonitor.spatialdataprocessor.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class HomeController {
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class.getName());
+
+    @RequestMapping("/")
+    public String index(@RequestHeader(value = "x-original-uri", required = false) Optional<String> xOriginalUri) {
+        if (xOriginalUri.isPresent() && !xOriginalUri.get().isEmpty()) {
+            String origWithoutTrailingSlash = xOriginalUri.get();
+            if (origWithoutTrailingSlash.charAt(origWithoutTrailingSlash.length() - 1) == '/') {
+                origWithoutTrailingSlash = origWithoutTrailingSlash.substring(0, origWithoutTrailingSlash.length() - 1);
+            }
+
+            String target = String.format("redirect:%s/swagger-ui.html", origWithoutTrailingSlash);
+            LOG.info("Redirecting to swagger UI: {}", target);
+            return target;
+        }
+
+        return "redirect:swagger-ui.html";
+    }
+}
