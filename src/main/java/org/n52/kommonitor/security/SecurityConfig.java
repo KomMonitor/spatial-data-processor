1e40d4fc5aecee87900f0c4ddf4863b392679c8d
speckij <speckij@posteo.net>
2022-10-07 13:58:56 +0200

    disable csrf to allow POST requests

--- src/main/java/org/n52/kommonitor/security/SecurityConfig.java
+++ src/main/java/org/n52/kommonitor/security/SecurityConfig.java
@@ -1,18 +1,17 @@
 package org.n52.kommonitor.security;
 
-
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @KeycloakConfiguration
-public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
-{
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     /**
      * Registers the KeycloakAuthenticationProvider with the authentication manager.
      */
@@ -31,15 +30,15 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
+    protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http
-                .authorizeRequests()
-                .antMatchers("/swagger-ui/*").permitAll()
-                .antMatchers("/v3/api-docs/swagger-config").permitAll()
-                .antMatchers("/specs/**").permitAll()
-                .antMatchers("/**").authenticated()
-                .anyRequest().denyAll();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/swagger-ui/*").permitAll()
+            .antMatchers("/v3/api-docs/swagger-config").permitAll()
+            .antMatchers("/specs/**").permitAll()
+            .antMatchers("/**").authenticated()
+            .anyRequest().denyAll();
     }
 }
---
+++ src/main/java/org/n52/kommonitor/spatialdataprocessor/config/ProcessConfiguration.java
@@ -0,0 +1,82 @@
+package org.n52.kommonitor.spatialdataprocessor.config;
+
+import java.util.function.Supplier;
+
+import org.n52.kommonitor.models.IsochronePruneProcessType;
+import org.n52.kommonitor.models.ProcessOverviewType;
+import org.n52.kommonitor.models.ProcessType;
+import org.n52.kommonitor.models.TestProcessType;
+import org.n52.kommonitor.spatialdataprocessor.process.IsochronePruneProcess;
+import org.n52.kommonitor.spatialdataprocessor.process.Process;
+import org.n52.kommonitor.spatialdataprocessor.process.TestProcess;
+import org.n52.kommonitor.spatialdataprocessor.util.datamanagement.DataManagementClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Static Registry providing information about all currently available Processes
+ */
+@Component
+public class ProcessRegistry {
+
+    public interface ProcessFactory<T extends ProcessType> {
+
+        String getProcessName();
+
+        Process<T> createProcess(T processType);
+
+        ProcessOverviewType getProcessOverview();
+    }
+
+    @Bean
+    public ProcessFactory<IsochronePruneProcessType> IsochronePruneProcessFactory(DataManagementClient dmc) {
+        return new ProcessFactory<>() {
+
+            private final String name = "isochrone-prune";
+
+            @Override
+            public String getProcessName() {
+                return name;
+            }
+
+            @Override
+            public Process<IsochronePruneProcessType> createProcess(IsochronePruneProcessType parameters) {
+                return new IsochronePruneProcess(parameters, dmc);
+            }
+
+            @Override
+            public ProcessOverviewType getProcessOverview() {
+                return new ProcessOverviewType()
+                        .id(IsochronePruneProcess.id)
+                        .name(name)
+                        .description("Process for cutting isochrones with spatial units");
+            }
+        };
+    }
+
+    @Bean
+    public ProcessFactory<TestProcessType> TestProcessFactory(DataManagementClient dmc) {
+        return new ProcessFactory<>() {
+
+            private final String name = "test";
+
+            @Override
+            public String getProcessName() {
+                return name;
+            }
+
+            @Override
+            public Process<TestProcessType> createProcess(TestProcessType processType) {
+                return new TestProcess(dmc);
+            }
+
+            @Override
+            public ProcessOverviewType getProcessOverview() {
+                return new ProcessOverviewType()
+                        .id(IsochronePruneProcess.id)
+                        .name(name)
+                        .description("Dummy Process for testing Job Queue System.");
+            }
+        };
+    }
+}
