20de7120234b805cab196ea5bb9f60cdfe013575
speckij <speckij@posteo.net>
2022-10-07 13:59:39 +0200

    refactor home-redirect to point to allowed location

--- src/main/java/org/n52/kommonitor/spatialdataprocessor/controller/HomeController.java
+++ src/main/java/org/n52/kommonitor/spatialdataprocessor/controller/HomeController.java
@@ -20,11 +20,11 @@ public class HomeController {
                 origWithoutTrailingSlash = origWithoutTrailingSlash.substring(0, origWithoutTrailingSlash.length() - 1);
             }
 
-            String target = String.format("redirect:%s/swagger-ui.html", origWithoutTrailingSlash);
+            String target = String.format("redirect:%s/swagger-ui/index.html", origWithoutTrailingSlash);
             LOG.info("Redirecting to swagger UI: {}", target);
             return target;
         }
 
-        return "redirect:swagger-ui.html";
+        return "redirect:swagger-ui/index.html";
     }
 }
