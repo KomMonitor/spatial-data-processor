7aac7c87e33a8cbd935acb0f662c899773879414
speckij <speckij@posteo.net>
2022-10-13 16:10:45 +0200

    remove deprecated keycloak adapter in favor of oauth2 resource server

--- src/main/java/org/n52/kommonitor/security/SecurityConfig.java
+++ src/main/java/org/n52/kommonitor/security/SecurityConfig.java
@@ -1,44 +1,52 @@
 package org.n52.kommonitor.security;
 
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@KeycloakConfiguration
-public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-    /**
-     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-     */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(keycloakAuthenticationProvider());
-    }
+@Configuration
+public class SecurityConfig {
+
+    //@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    //String jwkSetUri;
 
     /**
      * Defines the session authentication strategy.
      */
     @Bean
-    @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new NullAuthenticatedSessionStrategy();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.csrf().disable();
         http.authorizeRequests()
+            .antMatchers("/").permitAll()
             .antMatchers("/swagger-ui/*").permitAll()
             .antMatchers("/v3/api-docs/swagger-config").permitAll()
             .antMatchers("/specs/**").permitAll()
             .antMatchers("/**").authenticated()
             .anyRequest().denyAll();
+
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
     }
+
+    /*
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
+
+     */
 }
