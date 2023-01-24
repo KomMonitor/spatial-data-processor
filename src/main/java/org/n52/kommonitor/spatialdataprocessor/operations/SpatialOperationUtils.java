55ab640ef6a4fb122ac8b82dd61127ff6e59e7ab
Sebastian Drost <s.drost@52north.org>
2023-01-24 14:41:45 +0100

    Add additional intersection and filtering methods

--- src/main/java/org/n52/kommonitor/spatialdataprocessor/operations/SpatialOperationUtils.java
+++ src/main/java/org/n52/kommonitor/spatialdataprocessor/operations/SpatialOperationUtils.java
@@ -1,13 +1,11 @@
 package org.n52.kommonitor.spatialdataprocessor.operations;
 
-import org.geotools.data.crs.ReprojectFeatureReader;
-import org.geotools.data.geojson.GeoJSONWriter;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.factory.CommonFactoryFinder;
-import org.geotools.feature.FeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.*;
-import org.n52.kommonitor.spatialdataprocessor.process.IsochronePruneProcess;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.filter.Filter;
@@ -62,6 +60,23 @@ public class SpatialOperationUtils {
     }
 
     /**
+     * Calculates the polygonal intersection Geometry of three geometries
+     *
+     * @param geom1 First Geometry
+     * @param geom2 Second Geometry
+     * @param geom3 Third Geometry
+     * @return Intersecting Geometry
+     * @throws OperationException if one of the two geometries is not a Polygon or MultiPolygon
+     */
+    public Geometry polygonalIntersection(Geometry geom1, Geometry geom2, Geometry geom3) throws OperationException {
+        checkPolygonalType(geom1);
+        checkPolygonalType(geom2);
+        checkPolygonalType(geom3);
+        Geometry intersectionGeom = geom1.intersection(geom2);
+        return intersectionGeom.intersection(geom3);
+    }
+
+    /**
      * Calculates the proportion of intersection for the geometries of two SimpleFeatures. The result gives the
      * proportion of the intersecting geometry in relation to the geometry of the first SimpleFeature.
      *
@@ -90,6 +105,29 @@ public class SpatialOperationUtils {
         }
     }
 
+    /**
+     * Calculates the proportion of intersection of two Geometries, by using a subset of the first Geometry.
+     *
+     * The subset of the first Geometry will be used to calculate the intersection with the second Geometry. This new
+     * intersection then is used to calculate the proportion of intersection..
+     *
+     * @param geom1 First Geometry for which the proportion of intersection will be calculated
+     * @param geom2 Second Geometry
+     * @param subsetGeometry Subset og the first Geometry, which will be used to calculate and intersection with the
+     *                       second Geometry
+     * @return Proportion of intersection from 0 (no intersection) to 1 (full overlap).
+     * @throws OperationException if one of the two geometries is not a Polygon or MultiPolygon
+     */
+    public double polygonalIntersectionProportion(Geometry geom1, Geometry geom2, Geometry subsetGeometry) throws OperationException {
+        Geometry geom = polygonalIntersection(geom2, subsetGeometry);
+        // This is fail-safe for an empty geometry of the first SimpleFeature, since it prevents dividing by zero area.
+        if (geom.isEmpty()) {
+            return 0;
+        } else {
+            return geom.getArea() / geom1.getArea();
+        }
+    }
+
     public double polygonalIntersectionProportion(SimpleFeature feature, SimpleFeatureCollection featureCollection) {
         double intersection = 0;
         try (SimpleFeatureIterator iterator = featureCollection.features()) {
@@ -106,7 +144,24 @@ public class SpatialOperationUtils {
         return intersection;
     }
 
-    public SimpleFeatureCollection selectIntersectingFeatures(SimpleFeatureCollection fc, SimpleFeature feature)
+    public double polygonalIntersectionProportion(SimpleFeature feature, Geometry geom, SimpleFeatureCollection featureCollection) {
+        double intersection = 0;
+        try (SimpleFeatureIterator iterator = featureCollection.features()) {
+            while (iterator.hasNext()) {
+                SimpleFeature f2 = iterator.next();
+                try {
+                    intersection += polygonalIntersectionProportion((Geometry) feature.getDefaultGeometry(),
+                            geom, (Geometry)f2.getDefaultGeometry());
+                } catch (OperationException e) {
+                    LOGGER.warn("Could not calculate intersection between Geometry and feature {}", f2.getID());
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return intersection;
+    }
+
+    public SimpleFeatureCollection filterIntersectingFeatures(SimpleFeatureCollection fc, SimpleFeature feature)
             throws OperationException {
         FeatureType schema = fc.getSchema();
         if (!checkCrs(schema, feature.getFeatureType())) {
@@ -120,6 +175,34 @@ public class SpatialOperationUtils {
 
         Filter filter1 = ff.bbox(ff.property(geomName), bounds);
         Filter filter2 = ff.intersects(ff.property(geomName), ff.literal(feature.getDefaultGeometry()));
+        Filter filter = ff.and(filter1, filter2);
+
+        return fc.subCollection(filter);
+    }
+
+    /**
+     * Filters all SimpleFeatures of a SimpleFeatureCollection that intersects a given Geometry.
+     *
+     * Note, that Geometry must be in the same CRS as the SimpleFeatureCollection. Since Geometry is CRS-less,
+     * a {@link ReferencedEnvelope} is created from the {@link Envelope} of the given Geometry and the CRS of the
+     * SimpleFeatureCollection.
+     *
+     * @param fc SimpleFeatureCollection to filter SimpleFeature from
+     * @param geometry Geometry used for the intersection filter
+     * @return Subset SimpleFeatureCollection that only contains SimpleFeatures that intersect the given Geometry
+     */
+    public SimpleFeatureCollection filterIntersectingFeatures(SimpleFeatureCollection fc, Geometry geometry)
+            throws OperationException {
+        FeatureType schema = fc.getSchema();
+
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        String geomName = schema.getGeometryDescriptor().getLocalName();
+        Envelope env = geometry.getEnvelopeInternal();
+        ReferencedEnvelope bbox = new ReferencedEnvelope(env.getMinX(), env.getMinY(), env.getMaxX(), env.getMaxY(),
+                schema.getCoordinateReferenceSystem());
+
+        Filter filter1 = ff.bbox(ff.property(geomName), bbox);
+        Filter filter2 = ff.intersects(ff.property(geomName), ff.literal(geometry));
         Filter filter = ff.and(filter1, filter2);
 
         return fc.subCollection(filter);
@@ -181,7 +264,7 @@ public class SpatialOperationUtils {
     private boolean checkCrs(FeatureType schema1, FeatureType schema2) {
         CoordinateReferenceSystem crs1 = schema1.getCoordinateReferenceSystem();
         CoordinateReferenceSystem crs2 = schema2.getCoordinateReferenceSystem();
-        return crs1.getName().equals(crs2.getName());
+        return CRS.equalsIgnoreMetadata(crs1, crs2);
     }
 
 }
