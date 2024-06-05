ab29fd51fc470429bb9f664520083a48907d2f89
Sebastian Drost <s.drost@52north.org>
2024-06-05 15:50:25 +0200

    Fix CORS again

--- src/main/java/org/n52/kommonitor/security/SecurityConfig.java
+++ src/main/java/org/n52/kommonitor/security/SecurityConfig.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,9 +20,6 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
-
-    //@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    //String jwkSetUri;
 
     @Value("${kommonitor.spatial-data-processor.allowed-cors-origins}")
     private String[] allowedOrigins;
@@ -42,23 +38,20 @@ public class SecurityConfig {
             havingValue = "true",
             matchIfMissing = false)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> corsConfigurationSource())
-                .authorizeRequests((requests) -> requests
-                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui/*")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/swagger-config")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/specs/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/**")).authenticated()
-                        .anyRequest().denyAll()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
+        http.cors(Customizer.withDefaults());
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/swagger-ui/*").permitAll()
+                .antMatchers("/v3/api-docs/swagger-config").permitAll()
+                .antMatchers("/specs/**").permitAll()
+                .antMatchers("/**").authenticated()
+                .anyRequest().denyAll();
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
+
 
     @Bean
     @ConditionalOnProperty(
@@ -73,6 +66,7 @@ public class SecurityConfig {
                 .permitAll().and().build();
     }
 
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
