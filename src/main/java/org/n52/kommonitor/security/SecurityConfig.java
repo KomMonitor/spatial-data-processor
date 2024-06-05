f35270ca0cbe2e3ddaf45674cc4517afb305fef5
Sebastian Drost <s.drost@52north.org>
2024-06-05 14:12:09 +0200

    Add CORS config

--- src/main/java/org/n52/kommonitor/security/SecurityConfig.java
+++ src/main/java/org/n52/kommonitor/security/SecurityConfig.java
@@ -1,26 +1,32 @@
 package org.n52.kommonitor.security;
 
-import org.n52.kommonitor.dataloader.ShapeFileDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     //@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
     //String jwkSetUri;
+
+    @Value("${kommonitor.spatial-data-processor.allowed-cors-origins}")
+    private String[] allowedOrigins;
 
     /**
      * Defines the session authentication strategy.
@@ -36,17 +42,21 @@ public class SecurityConfig {
             havingValue = "true",
             matchIfMissing = false)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.csrf().disable();
-        http.authorizeRequests()
-            .antMatchers("/").permitAll()
-            .antMatchers("/swagger-ui/*").permitAll()
-            .antMatchers("/v3/api-docs/swagger-config").permitAll()
-            .antMatchers("/specs/**").permitAll()
-            .antMatchers("/**").authenticated()
-            .anyRequest().denyAll();
-
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> corsConfigurationSource())
+                .authorizeRequests((requests) -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui/*")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/swagger-config")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/specs/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/**")).authenticated()
+                        .anyRequest().denyAll()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
 
@@ -62,26 +72,15 @@ public class SecurityConfig {
                 .anyRequest()
                 .permitAll().and().build();
     }
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("*");
-//                        .allowCredentials(true);
-            }
-        };
+
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
-    /*
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-    }
-
-     */
 }
